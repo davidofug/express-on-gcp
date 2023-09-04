c7207ecd5d424e97c4583aee87b8234a23a5ffff
David Wampamba <davidwampamba@gmail.com>
2023-09-04 13:52:24 +0300

    Visit Ruboni endpoint

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
+app.get("/api/v1/visit-ruboni", (req, res) => {
+	res.send("Visit Ruboni Endpoint");
+});
+
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
